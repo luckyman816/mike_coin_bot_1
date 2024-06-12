c5370360a4d69875252def52f0040ca72cec5f31
Senior Coder <agilecoder2040@gmail.com>
2024-06-12 15:43:39 +0900

    update loading page and connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { useState, useEffect } from "react";
 import Home from "./page/Home";
 import Ranking from "./page/Ranking";
 import Quest from "./page/Quest";
 import { ToastContainer } from "react-toastify";
 import Footer from "./component/Footer";
 import Layout from "./Layout";
-import Header from "./component/Header";
+import Loading from "./component/Loading";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 7000);
+  }, []);
   return (
     <Router>
+      {loading ? (
+        <Loading />
+      ) : (
         <div className="App w-[700px] h-[75vh] max-sm:w-[750px] max-sm:h-[70vh]">
-          <Header />
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
@@ -23,6 +33,7 @@ function App() {
           <ToastContainer />
           <Footer />
         </div>
+      )}
     </Router>
   );
 }
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -3,7 +3,10 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import CountDate from "../component/CountDate";
 import ProgressBar from "../component/ProgressBar";
+import { TonConnectButton, useTonWallet } from "@tonconnect/ui-react";
 function Home() {
+  const wallet = useTonWallet();
+  console.log("--------->", wallet?.device);
   const [token, setToken] = useState<number>(2000);
   const [remainedEnergy, setRemainedEnergy] = useState(500);
   function formatNumberWithCommas(number: number, locale = "en-US") {
@@ -13,8 +16,8 @@ function Home() {
   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
     const rect = event.currentTarget.getBoundingClientRect();
-    const x = Math.random()* (event.clientX - rect.left);
-    const y = Math.random()*(event.clientY - rect.top);
+    const x = Math.random() * (event.clientX - rect.left);
+    const y = Math.random() * (event.clientY - rect.top);
 
     const styleElement = document.createElement("style");
     document.head.appendChild(styleElement);
@@ -29,17 +32,17 @@ function Home() {
     newDiv.textContent = "+1";
     newDiv.style.backgroundImage = "url('image/dollar.png')";
     newDiv.style.backgroundRepeat = "no-repeat";
-    newDiv.style.backgroundPosition = "center"; 
-    newDiv.style.fontSize = "20px"
-    newDiv.style.paddingLeft = "30px"
-    newDiv.style.display = 'flex'
-    newDiv.style.justifyContent = 'center'
-    newDiv.style.alignItems= 'center'
-    newDiv.style.backgroundSize = 'cover'
-    newDiv.style.width = "40px"
-    newDiv.style.height = "40px"
+    newDiv.style.backgroundPosition = "center";
+    newDiv.style.fontSize = "20px";
+    newDiv.style.paddingLeft = "30px";
+    newDiv.style.display = "flex";
+    newDiv.style.justifyContent = "center";
+    newDiv.style.alignItems = "center";
+    newDiv.style.backgroundSize = "cover";
+    newDiv.style.width = "40px";
+    newDiv.style.height = "40px";
     newDiv.style.position = "absolute";
-    newDiv.style.left = `${x+50}px`;
+    newDiv.style.left = `${x + 50}px`;
     newDiv.style.top = `${y}px`;
     newDiv.style.color = "#58E1E2";
     newDiv.className =
@@ -77,12 +80,16 @@ function Home() {
   console.log("imgStatus", imgStatus);
 
   return (
-    <div>
+    <div className="mt-3">
       <ToastContainer />
+      <div className="w-full flex justify-center">
+        <TonConnectButton />
+      </div>
+
       <CountDate date={3} />
       <div
         id="mainWindow"
-        className="relative mt-10 flex flex-col items-center justify-center w-full h-[60vh] mb-9"
+        className="relative mt-5 flex flex-col items-center justify-center w-full h-[60vh] mb-9"
       >
         <div className="flex flex-col justify-center items-center mb-7">
           <h3 className="text-xl font-bold text-[#939392]">$GoXP balance</h3>
@@ -101,7 +108,7 @@ function Home() {
               remainedEnergy > 0
                 ? "cursor-pointer"
                 : "cursor-not-allowed opacity-50"
-            } ${imgStatus ? "opacity-80" : "opacity-100"}`}
+            } ${imgStatus ? " border-[5px]" : "border-0"}`}
             ref={bodyRef}
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseLeave}
