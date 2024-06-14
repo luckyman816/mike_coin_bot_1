8ebc35847a905a701a8ffdef63da51d79ecfbf14
Senior Coder <agilecoder2040@gmail.com>
2024-06-15 06:28:01 +0900

    d

--- src/component/QuestList.tsx
+++ src/component/QuestList.tsx
@@ -16,6 +16,21 @@ export default function QuestList() {
           </div>
         </div>
       </div>
+      <div className="flex items-center h-36 max-sm:h-24 justify-between px-3 py-2 my-4 bg-[#363636] rounded-lg">
+        <div className="flex justify-start items-center">
+          <img src="image/bonus.png" alt="" className=" w-14 h-14" />
+          <div className=" flex flex-col justify-center items-center">
+            <div className="flex justify-start items-start text-white font-bold">
+              Invite a friend with Telegram premium
+            </div>
+            <div className="flex justify-start ml-2">
+              <img src="image/dollar.png" alt="" className=" w-5 h-5" />
+              <span className=" text-amber-400">+500</span>
+              <span>&nbsp;&nbsp;&nbsp;for you and your friend</span>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="flex flex-col justify-start items-start">
         <h2 className="text-white text-sm">List of your friends</h2>
         <div className=" rounded-[20px] bg-[#525252]">
