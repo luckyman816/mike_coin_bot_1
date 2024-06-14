422ec419e9218b68965ef28df9df5c6e35df5381
Senior Coder <agilecoder2040@gmail.com>
2024-06-15 01:52:41 +0900

    -n

--- src/page/Boost.tsx
+++ src/page/Boost.tsx
@@ -2,7 +2,7 @@ export default function Boost() {
   return (
     <div className="Boost max-w-full mx-auto text-white h-[75vh] max-sm:h-[82vh] mt-8">
       <div className="md:w-full h-[65vh] mx-auto flex flex-col justify-center ">
-        <div className="max-h-[50vh] max-sm:max-h-[50vh] overflow-auto">
+        <div className="flex flex-col justify-center items-center">
           <h1 className="text-3xl mb-3  max-w-[500px] mx-auto text-start text-white flex justify-center">
             Your Balance
           </h1>
