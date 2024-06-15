9544a66d05a9042c137728d1a2bd86c2b511c422
Senior Coder <agilecoder2040@gmail.com>
2024-06-16 01:55:25 +0900

    d

--- src/component/QuestList.tsx
+++ src/component/QuestList.tsx
@@ -17,9 +17,9 @@ export default function QuestList() {
     });
   };
   return (
-    <div className="max-h-[75vh] max-sm:max-h-[75vh] overflow-auto">
+    <div className="max-h-[75vh] max-sm:max-h-[75vh] overflow-auto p-5">
       <div className="flex flex-col justify-center items-center">
-        <h1 className=" text-white ">Invite friends!</h1>
+        <h1 className=" text-white text-2xl">Invite friends!</h1>
         <p className=" text-white">You and your friend will receive bonuses</p>
       </div>
       <div className="flex items-center h-36 max-sm:h-24 justify-between px-3 py-2 my-4 bg-[#363636] rounded-[20px]">
