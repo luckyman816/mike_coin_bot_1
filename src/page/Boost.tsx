6ba3d47043da7ad20f7e68c85d397bba71258ce1
Senior Coder <agilecoder2040@gmail.com>
2024-06-16 06:40:50 +0900

    d

--- src/page/Boost.tsx
+++ src/page/Boost.tsx
@@ -15,6 +15,7 @@ export default function Boost() {
     setLimit(limit_state)
   }, [tokenState, username_state, limit_state])
   const handleFullEnergy = () => {
+    console.log("-----full energy💰🏆💪------>", limit)
     dispatch(updateEnergy(username, limit))
     setIsModalOpen(false);
   }
