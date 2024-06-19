57ab2aff662d3acb7056f9ef656b9bfffb93aca5
Senior Coder <agilecoder2040@gmail.com>
2024-06-19 16:06:53 +0900

    d

--- src/page/Task.tsx
+++ src/page/Task.tsx
@@ -1,7 +1,9 @@
 export default function Task() {
   return (
     <div className="Ranking max-w-full mx-auto text-white h-[75vh] max-sm:h-[82vh] mt-8">
-      <img src="image/dollar.png" alt="" className=" w-20 h-20" />
+      <div className="flex justify-center">
+        <img src="image/dollar.png" alt="" className=" w-20 h-20" />
+      </div>
 
       <div className="flex justify-center p-7">
         <div className="flex items-center h-24 max-sm:h-24 justify-between px-3 py-2 my-4 bg-[#363636] rounded-[20px]">
