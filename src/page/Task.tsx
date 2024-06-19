1eb9435e4447f25c791eb9e9645159b793505266
Senior Coder <agilecoder2040@gmail.com>
2024-06-19 15:57:09 +0900

    d

---
+++ src/page/Task.tsx
@@ -0,0 +1,50 @@
+export default function Task() {
+  return (
+    <div className="Ranking max-w-full mx-auto text-white h-[75vh] max-sm:h-[82vh] mt-8">
+       <img src="image/dollar.png" alt="" className=" w-20 h-20"/>
+ 
+      <div className="flex items-center h-24 max-sm:h-24 justify-between px-3 py-2 my-4 bg-[#363636] rounded-[20px]">
+        <div className="flex justify-start items-center">
+          <img src="image/telegram.png" alt="" className=" w-14 h-14" />
+          <div className=" flex flex-col justify-start">
+            <div className="text-left justify-start items-center text-white ml-3 font-bold">
+              Join Our TG Group
+            </div>
+            <div className="flex justify-start items-center ml-2">
+              <img src="image/dollar.png" alt="" className=" w-5 h-5" />
+              <span className=" text-amber-400">+5000</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center h-24 max-sm:h-24 justify-between px-3 py-2 my-4 bg-[#363636] rounded-[20px]">
+        <div className="flex justify-start items-center">
+          <img src="image/telegram.png" alt="" className=" w-14 h-14" />
+          <div className=" flex flex-col justify-start">
+            <div className="text-left justify-start items-center text-white ml-3 font-bold">
+              Subscribe Our TG Channel
+            </div>
+            <div className="flex justify-start items-center ml-2">
+              <img src="image/dollar.png" alt="" className=" w-5 h-5" />
+              <span className=" text-amber-400">+5000</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center h-24 max-sm:h-24 justify-between px-3 py-2 my-4 bg-[#363636] rounded-[20px]">
+        <div className="flex justify-start items-center">
+          <img src="image/twitter.png" alt="" className=" w-14 h-14" />
+          <div className=" flex flex-col justify-start">
+            <div className="text-left justify-start items-center text-white ml-3 font-bold">
+              Follow our X account
+            </div>
+            <div className="flex justify-start items-center ml-2">
+              <img src="image/dollar.png" alt="" className=" w-5 h-5" />
+              <span className=" text-amber-400">+5000</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
