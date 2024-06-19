98367a728bdb0885b94966244f10f1fb14b11574
Senior Coder <agilecoder2040@gmail.com>
2024-06-19 16:08:29 +0900

    d

--- src/page/Task.tsx
+++ src/page/Task.tsx
@@ -5,7 +5,7 @@ export default function Task() {
         <img src="image/dollar.png" alt="" className=" w-20 h-20" />
       </div>
 
-      <div className="flex justify-center p-7">
+      <div className="flex flex-col justify-center p-7">
         <div className="flex items-center h-24 max-sm:h-24 justify-between px-3 py-2 my-4 bg-[#363636] rounded-[20px]">
           <div className="flex justify-start items-center">
             <img src="image/telegram.png" alt="" className=" w-14 h-14" />
