5258e00cc89d8d9eae579d4f0998d185524ed7ef
Senior Coder <agilecoder2040@gmail.com>
2024-06-20 06:08:07 +0900

    d

--- src/page/Task.tsx
+++ src/page/Task.tsx
@@ -28,13 +28,37 @@ export default function Task() {
             toast.warning("You have already received bonus!");
           }
         } else {
-          toast.warning("You didn't join Telegram yet! Please join again");
+          toast.warning("You didn't join Telegram Group yet! Please join again");
         }
       });
     } catch (error) {
       console.log(error);
     }
   };
+  const handleSubscribeTelegramChannel = async() => {
+    try {
+      await axios.post(`/earnings/${username}`).then((res) => {
+        if(res.data.subscribeTelegram.status) {
+          if(!res.data.subscribeTelegram.earned) {
+            dispatch(updateBalance(username, balance + 1000)).then(() => {
+              axios.post(`/earnings/update/subscribeTelegram/${username}`, {
+                status: true,
+                earned: true,
+              });
+              toast.success("You have received +1000 coins successfully!");
+            })
+          } else {
+            toast.warning("You have already received bonus!");
+          }
+        } else {
+          toast.warning("You didn't subscribe Telegram Channel yet! Please join again");
+        }
+      })
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className="Ranking max-w-full mx-auto text-white h-[75vh] max-sm:h-[82vh] mt-8">
       <ToastContainer />
@@ -61,7 +85,7 @@ export default function Task() {
             </div>
           </div>
         </div>
-        <div className="flex items-center h-24 max-sm:h-24 justify-between px-3 py-2 my-4 bg-[#363636] hover:bg-zinc-500 rounded-[20px]">
+        <div className="flex items-center h-24 max-sm:h-24 justify-between px-3 py-2 my-4 bg-[#363636] hover:bg-zinc-500 rounded-[20px]" onClick={handleSubscribeTelegramChannel}>
           <div className="flex justify-start items-center">
             <img src="image/telegram.png" alt="" className=" w-14 h-14" />
             <div className=" flex flex-col justify-start">
