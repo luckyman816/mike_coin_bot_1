c3a81ef2f825219e4544513acda59485d0852079
Senior Coder <agilecoder2040@gmail.com>
2024-06-27 23:54:40 +0900

    d

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -1,14 +1,47 @@
-import "./Loading.css"
+import "./Loading.css";
 const Loading = () => {
-    return (
-        <div className="loadingspinner">
-            <div id="square1" style={{backgroundImage: "url('image/mike_1.png')", backgroundSize: "cover"}}></div>
-            <div id="square2" style={{backgroundImage: "url('image/mike_2.png')", backgroundSize: "cover"}}></div>
-            <div id="square3" style={{backgroundImage: "url('image/mike_3.png')", backgroundSize: "cover"}}></div>
-            <div id="square4" style={{backgroundImage: "url('image/mike_4.png')", backgroundSize: "cover"}}></div>
-            <div id="square5" style={{backgroundImage: "url('image/mike_5.png')", backgroundSize: "cover"}}></div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center gap-12">
+        <img src="/image/title.png" alt="" />
+      <div className="loadingspinner">
+        <div
+          id="square1"
+          style={{
+            backgroundImage: "url('image/mike_1.png')",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div
+          id="square2"
+          style={{
+            backgroundImage: "url('image/mike_2.png')",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div
+          id="square3"
+          style={{
+            backgroundImage: "url('image/mike_3.png')",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div
+          id="square4"
+          style={{
+            backgroundImage: "url('image/mike_4.png')",
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div
+          id="square5"
+          style={{
+            backgroundImage: "url('image/mike_5.png')",
+            backgroundSize: "cover",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
-export default Loading
+export default Loading;
