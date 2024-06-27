863f9316950f33df8bb587935832d18361cb8665
Senior Coder <agilecoder2040@gmail.com>
2024-06-27 23:55:45 +0900

    d

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -2,7 +2,7 @@ import "./Loading.css";
 const Loading = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-12">
-        <img src="/image/title.png" alt="" />
+        <img src="/image/title.png" alt="" className=" w-[80%] h-auto"/>
       <div className="loadingspinner">
         <div
           id="square1"
