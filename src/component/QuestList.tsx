4674efa9a421d2d9225f0cc519e5b0630917d405
Senior Coder <agilecoder2040@gmail.com>
2024-06-29 06:22:35 +0900

    d

--- src/component/QuestList.tsx
+++ src/component/QuestList.tsx
@@ -30,41 +30,77 @@ export default function QuestList() {
   return (
     <div className="max-h-[75vh] max-sm:max-h-[75vh] overflow-auto p-5 flex flex-col justify-center items-center gap-2">
       <ToastContainer />
-      <div className="flex justify-center items-center">
-        <h1 className=" text-white text-3xl" style={{fontFamily: "spicy"}}>{friends.length} Refferals</h1>
-        <h1 className=" text-[#33CC66] text-3xl" style={{fontFamily: "poppins"}}>+ 1000</h1>
-
+      <div className="flex flex-col justify-center items-center">
+        <h1 className=" text-white text-3xl" style={{ fontFamily: "spicy" }}>
+          {friends.length} Refferals
+        </h1>
+        <h1
+          className=" text-[#33CC66] text-2xl"
+          style={{ fontFamily: "poppins" }}
+        >
+          + 1000
+        </h1>
       </div>
       <div className="flex justify-center items-center align-middle w-full mt-8">
         <div className="w-[90%] bg-gradient-to-r from-[#567481] to-[#2D4047]  text-white rounded-[20px] flex items-center justify-center p-5">
           <div className="flex flex-col justify-center items-center gap-2">
-            <span className="flex justify-center items-cente text-2xl" style={{fontFamily: "spicy"}}>
+            <span
+              className="flex justify-center items-cente text-2xl"
+              style={{ fontFamily: "spicy" }}
+            >
               My invite link
             </span>
-            <span className="text-[#00E9F8]" style={{maxWidth: "150px", overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap"}}>
+            <span
+              className="text-[#00E9F8]"
+              style={{
+                maxWidth: "150px",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+              }}
+            >
               {textToCopy}
             </span>
           </div>
           <CopyToClipboard text={textToCopy} onCopy={handleCopy}>
             <div className="bg-gradient-to-r flex justify-center items-center gap-5 from-[#806FC0] to-[#14D6F0] p-3 rounded-[8px]">
-              <img src="/image/assets/copy.png" alt="" className="w-4 h-4"/>
-              <h2 className="text-sm text-[white]" style={{ fontFamily: "poppins" }}>Copy</h2>
+              <img src="/image/assets/copy.png" alt="" className="w-4 h-4" />
+              <h2
+                className="text-sm text-[white]"
+                style={{ fontFamily: "poppins" }}
+              >
+                Copy
+              </h2>
             </div>
           </CopyToClipboard>
         </div>
       </div>
       <div className="flex flex-col justify-start items-start">
-        <h2 className="text-white text-sm">List of your friends</h2>
-        <div className=" rounded-[20px] bg-[#525252] w-full h-16 flex justify-center items-center">
-          <select
-            name="choice"
-            className=" border-none bg-[#525252] text-sm w-[100%] mx-9 focus:border-none focus:outline-none"
-          >
-            {friends.map((it) => {
-              return <option value={it.friend}>{it.friend}</option>;
-            })}
-          </select>
-        </div>
+        <h2 className="text-white text-3xl" style={{ fontFamily: "spicy" }}>
+          My Refferals
+        </h2>
+        {friends.length == 0 ? (
+          <div className="flex flex-col justify-center items-center">
+            <h2
+              className="text-[white] text-xl"
+              style={{ fontFamily: "poppins" }}
+            >
+              You don't have refferal!
+            </h2>
+            <img src="image/assets/noRefferal.png" alt="" className="w-4 h-4" />
+          </div>
+        ) : (
+          <div className="max-h-[40vh] max-sm:max-h-[40vh] overflow-auto">
+            {friends.map((item, index) => (
+              <div
+                key={index}
+                className={`flex ${
+                  index > 0 && "my-3"
+                } px-3 py-2 items-center bg-[#363636] rounded-lg w-[70%] text-[white]`}
+              >{item}</div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
