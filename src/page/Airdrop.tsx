119c96cefa42d32282da28ef47117e7d669ef523
Senior Coder <agilecoder2040@gmail.com>
2024-06-29 22:22:38 +0900

    d

--- src/page/Airdrop.tsx
+++ src/page/Airdrop.tsx
@@ -1,12 +1,20 @@
 export default function Airdrop() {
   return (
     <div className="Ranking max-w-full mx-auto text-white max-sm:h-[78vh] mt-8">
-        <div className="flex flex-col justify-center items-center gap-5">
-            <img src="image/assets/wallet.png" alt="" className="w-24 h-24"/>
-            <h2 className="text-sm text-[white]">Submit your bep20 wallet address</h2>
-            <input type="text" className=" bg-gradient-to-r from-[#556166] to-[#293337] p-4 border border-[white] border-solid rounded-[10px] width-[80%]"/>
-            <button></button>
-        </div>
+      <div className="flex flex-col justify-center items-center gap-5">
+        <img src="image/assets/wallet.png" alt="" className="w-24 h-24" />
+        <h2 className="text-sm text-[white]">
+          Submit your bep20 wallet address
+        </h2>
+        <input
+          type="text"
+          className=" bg-gradient-to-r from-[#556166] to-[#293337] p-4 border border-[white] border-solid rounded-[10px] w-[80%]"
+        />
+        <button className="bg-[#33CC66] text-[white] w-[30%] rounded-[10px]">
+          <img src="image/icon/submit.png" alt="" className=" w-3 h-3" />
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
