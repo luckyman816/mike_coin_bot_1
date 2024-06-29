d0f467d8ad1ae4d313a094158147f9ba0f75d019
Senior Coder <agilecoder2040@gmail.com>
2024-06-29 22:28:28 +0900

    d

--- src/page/Airdrop.tsx
+++ src/page/Airdrop.tsx
@@ -10,8 +10,8 @@ export default function Airdrop() {
           type="text"
           className=" bg-gradient-to-r from-[#556166] to-[#293337] p-4 border border-[white] border-solid rounded-[10px] w-[80%]"
         />
-        <button className="bg-[#33CC66] text-[white] w-[20%] rounded-[10px] flex items-center text-2xl">
-          <img src="image/icon/submit.png" alt="" className=" w-8 h-8 " />
+        <button className="bg-[#33CC66] text-[white] w-[20%] rounded-[10px] flex items-center text-xl">
+          <img src="image/icon/submit.png" alt="" className=" w-6 h-6 " />
           Submit
         </button>
       </div>
