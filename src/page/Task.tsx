fcbf32388553ea50d1f6fdcf77c5ea459e45c772
Senior Coder <agilecoder2040@gmail.com>
2024-06-30 23:51:03 +0900

    d

--- src/page/Task.tsx
+++ src/page/Task.tsx
@@ -107,10 +107,10 @@ export default function Task() {
             Send your vibe to Mike's TG group and earn some coins
           </h2>
           <div className="flex justify-center items-center gap-3">
-            <button className="bg-[#3C4648] text-[white] w-[20%] rounded-[10px] flex justify-center items-center text-[16px] gap-2 border-[1px] border-[#33CC66] border-solid">
+            <button className="bg-[#3C4648] text-[white] w-[40%] rounded-[10px] flex justify-center items-center text-[16px] gap-2 border-[1px] border-[#33CC66] border-solid">
               Let's Go
             </button>
-            <button className="bg-[#33CC66] text-[white] w-[20%] rounded-[10px] flex justify-center items-center text-[16px] gap-2 border-[1px] border-white border-solid">
+            <button className="bg-[#33CC66] text-[white] w-[40%] rounded-[10px] flex justify-center items-center text-[16px] gap-2 border-[1px] border-white border-solid">
               Check
             </button>
           </div>
