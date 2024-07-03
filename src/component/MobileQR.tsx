feab72ed63c885a45b7e4d3e3f8fdac43f5c3eca
Senior Coder <agilecoder2040@gmail.com>
2024-07-04 04:34:50 +0900

    d

--- src/component/MobileQR.tsx
+++ src/component/MobileQR.tsx
@@ -1,9 +1,9 @@
 const MobileQR = () => {
     return (
         <div className="flex flex-col justify-center items-center gap-3">
-            <h2 className="text-white text-[2xl]">Play on the Mobile</h2>
+            <h2 className="text-white text-2xl font-bold">Play on the Mobile</h2>
             <img src="/image/qr.png" alt="" className="w-[60vw] h-auto"/>
-            <h2 className="text-white text-[2xl">@ monster_mysterybot</h2>
+            <h2 className="text-white text-2xl font-bold">@ monster_mysterybot</h2>
         </div>
     )
 }
