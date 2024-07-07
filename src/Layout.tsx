4a91c5df13289176c094db0eb29e10903af6820b
Senior Coder <agilecoder2040@gmail.com>
2024-07-08 04:30:50 +0900

    d

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,23 @@
+import React, { useRef, RefObject } from "react";
 import { Outlet } from "react-router-dom";
 export default function Layout() {
+  const mainScreenRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
+
+  const handleTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
+    if (event.touches.length === 2) {
+      event.preventDefault();
+    }
+  };
   return (
-    <div>
+    <div
+      ref={mainScreenRef}
+      onTouchMove={handleTouchMove}
+      style={{
+        width: "100%",
+        height: "100%",
+        touchAction: "none",
+      }}
+    >
       <Outlet />
     </div>
   );
