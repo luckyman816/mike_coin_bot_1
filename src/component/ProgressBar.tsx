d32ff14decfbca548eac2dda92e994abb546f54e
Senior Coder <agilecoder2040@gmail.com>
2024-07-14 23:56:10 +0900

    d

--- src/component/ProgressBar.tsx
+++ src/component/ProgressBar.tsx
@@ -2,7 +2,7 @@ export default function ProgressBar({ value }: { value: number }) {
   return (
     <div className="flex items-center justify-center">
       <div
-        className="w-[75vw] h-4 border-[#E39431] bg-[#2F2F2F] border-[1px] rounded-full flex items-center"
+        className="w-[80vw] h-4 border-[#E39431] bg-[#2F2F2F] border-[1px] rounded-full flex items-center"
       >
         <div
           className="bg-gradient-to-r from-[#A07FF1] to-[#06E1F4] h-4 rounded-full"
