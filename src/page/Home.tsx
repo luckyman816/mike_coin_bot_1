04c990ed31aac8585d48befe8e58462bbeb1bcb4
Senior Coder <agilecoder2040@gmail.com>
2024-07-15 03:47:56 +0900

    change the margin

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -134,7 +134,7 @@ function Home() {
   return (
     <div className=" mt-2 h-full">
       <ToastContainer />
-      <div className="flex justify-around items-center px-3">
+      <div className="flex justify-around items-center px-3 w-full">
         <h3
           className="text-2xl font-bold text-[white]"
           style={{ fontFamily: "spicy" }}
@@ -143,7 +143,7 @@ function Home() {
         </h3>
         <img src="image/assets/icon.png" alt="" className=" w-10 h-10" />
       </div>
-      <div className="flex gap-2 justify-center items-center">
+      <div className="flex gap-2 justify-center items-center w-full">
         <div className="bg-gradient-to-r from-[#567481] to-[#2D4047] flex flex-col justify-center items-center p-2 rounded-[8px] w-[25%]">
           <h2 className=" text-sm text-[#F8B219]">Earn Per Tap</h2>
           <div className="flex justify-center items-center">
@@ -169,7 +169,7 @@ function Home() {
         id="mainWindow"
         className="relative mt-3 flex flex-col items-center justify-between w-full h-[72vh] mb-4"
       >
-        <div className="flex justify-center items-center mb-7 bg-gradient-to-r from-[#A07FF1] to-[#06E1F4] w-[80%] rounded-[10px] p-2">
+        <div className="flex justify-center items-center mb-7 bg-gradient-to-r from-[#A07FF1] to-[#06E1F4] w-[90%] rounded-[10px] p-2">
           <div className="flex justify-center items-center">
             <img src="image/assets/mkt.png" alt="" className=" w-10 h-10" />
             <h1
