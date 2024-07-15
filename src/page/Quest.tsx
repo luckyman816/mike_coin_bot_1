8a914d668868d99fd174ba0bd30a032e0f6987b2
Senior Coder <agilecoder2040@gmail.com>
2024-07-15 12:36:41 +0900

    update refferal page

--- src/page/Quest.tsx
+++ src/page/Quest.tsx
@@ -1,9 +1,11 @@
+import Footer from "../component/Footer";
 import QuestList from "../component/QuestList";
 
 export default function Quest() {
   return (
-    <div className="max-w-full mx-auto text-white max-sm:h-[75vh] mt-12 mb-[13%]">
+    <div className="h-full w-full flex flex-col justify-between items-center mt-11">
       <QuestList />
+      <Footer/>
     </div>
   );
 }
