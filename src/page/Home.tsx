eb092dd89bfa1748d1db0a2656d8add35c8bf70d
Senior Coder <agilecoder2040@gmail.com>
2024-07-16 01:34:54 +0900

    udpate commite of udpading

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -124,19 +124,15 @@ function Home() {
       handleClick(event);
     }
   };
-  const handleMultiTouchStart = (event: TouchEvent) => {
-    // Iterate over each touch point
-    Array.from(event.touches).forEach((touch) => {
-      console.log("Touch's current position:", touch);
-      // Call handleClick for each touch point
-      handleClick(event);
-    });
-  };
   const handleTouch = (event: any) => {
     const length = event.touches.length;
     if (remainedEnergy - length >= 0 && length >= 1) {
       setRemainedEnergy(remainedEnergy - length);
-      handleMultiTouchStart(event);
+      Array.from(event.touches).forEach((touch) => {
+        console.log("Touch's current position:", touch);
+        // Call handleClick for each touch point
+        handleClick(event);
+      });
     }
   };
   const handleMouseDown = () => {
